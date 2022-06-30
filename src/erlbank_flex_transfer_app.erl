3b35a0796566aab4cc65be6e7133ecbe491c1aa3
Mike Sperber <sperber@deinprogramm.de>
2022-06-30 15:53:22 +0200

    Take ACCOUNTS_NODE from environment.

--- src/erlbank_flex_transfer_app.erl
+++ src/erlbank_flex_transfer_app.erl
@@ -2,6 +2,13 @@
 -behaviour(application).
 -export([start/2, stop/1]).
 
+check_is_set(Var) ->
+    case os:getenv(Var) of
+        false ->
+            io:format("Missing var ~s~n", [Var]),
+            halt(1);
+        _ -> ok
+    end.
 
 start_cowboy() ->
     %% Cowboy test code
@@ -16,10 +23,15 @@ start(_StartType, _StartArgs) ->
 
     lager:info("Starting transfer-service: ~p~n", [node()]),
 
+    check_is_set("ACCOUNTS_HOST"),
+    AccountNode = list_to_atom("accounts@" ++ os:getenv("ACCOUNTS_HOST")),
+
     start_cowboy(),
     database:init_database(),
 
     Res = erlbank_flex_transfer_sup:start_link(),
+    transfer_feed:start_link(dontCare),
+    transfer:start({account_feed, AccountNode}),
     lager:info("Started transfer feed: ~p~n", [node()]),
     Res.
 
